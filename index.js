e4d13cce3d27df1ec54f5c4c0ff15a426a0bb9ca
Iuri Moura <48696872+msiuri@users.noreply.github.com>
2022-07-21 11:46:23 -0300

    Inserido conexão com banco na cloud

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+
+// const url = "mongodb://localhost:27017"
+const url = "mongodb+srv://msiuri:11N0AHASawZvWm8tb@cluster0.xxbynb6.mongodb.net";
 const dbName = "ocean_bancodados_19_07_2022";
 
 async function main() {
-  /*
+  
   console.log("Conectando ao banco de dados...");
 
   const client = await MongoClient.connect(url);
@@ -13,7 +15,7 @@ async function main() {
   const collection = db.collection("herois");
 
   console.log("Banco de dados conectado com sucesso!");
-  */
+  
   // Aplicação Backend com Express
 
   const app = express();
